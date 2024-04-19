e9a0b32e26fd0b4198621d94e9d59cfe17329472
Tuxtamuradov <154896763+Tuxtamuradov@users.noreply.github.com>
2024-04-19 09:44:29 +0500

    app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        
+        <h2> jalol </h2>
         <a
           className="App-link"
           href="https://reactjs.org"
